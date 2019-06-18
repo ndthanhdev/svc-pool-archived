8151ac7c7e753fb04228ce666e57c8f889196774
Thanh Nguyen <ndthanhdev@outlook.com>
2019-06-18 23:30:56 +0700

    testing plugin-pool-react

--- packages/plugin-pool-react/jest.config.js
+++ packages/plugin-pool-react/jest.config.js
@@ -2,4 +2,7 @@ const base = require('../../configs/jest.config')
 
 module.exports = {
   ...base,
+  testEnvironment: 'jsdom',
+
+  setupFilesAfterEnv: ['./test/setupTests.ts'],
 }
--- packages/plugin-pool-react/test/index.test.ts
+++
@@ -1,3 +0,0 @@
-test('test placeholder', () => {
-  expect(true).toBeTruthy
-})
