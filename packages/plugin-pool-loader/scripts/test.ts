5270abcdc7bb5561a9de6163497b6b7b86258f70
ndthanhdev <ndthanhdev@outlook.com>
2019-08-13 01:23:41 +0700

    [WIP]

--- packages/plugin-pool-loader/scripts/test.ts
+++ packages/plugin-pool-loader/scripts/test.ts
@@ -1,9 +1,21 @@
 import execa from 'execa'
+import { Readable } from 'stream'
+import kill from 'tree-kill'
 ;(async () => {
-  const testServer = execa('hs')
-  testServer.stdout.pipe(process.stdout);
-  setTimeout(() => {
-    testServer.kill()
-  }, 3000)
-  //=> 'unicorns'
+  const testAssetsServer = (function() {
+    const r = execa('serve', ['./test/assets/'])
+    const stdout = <Readable>r.stdout
+    stdout.pipe(process.stdout)
+    const stderr = <Readable>r.stderr
+    stderr.pipe(process.stderr)
+    return r
+  })()
+
+  
+
+  function cleanUp() {
+    kill(testAssetsServer.pid)
+  }
+
+  process.on('SIGINT', cleanUp)
 })()
