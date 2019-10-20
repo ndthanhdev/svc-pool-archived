2c57c7d02fa9b3f87e54bf5d82b9c8115f7fb91c
ndthanhdev <ndthanhdev@outlook.com>
2019-10-20 19:29:01 +0700

    Updates

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
 import { IServiceDefinition } from './interfaces/ServiceDefinition';
 import { Point } from './interfaces/Point';
-export { createDefinitionPool, PluginPool } from './createDefinitionPool';
+export { createDefinitionPool, PluginPool, DefinitionPool } from './createDefinitionPool';
 export declare const createPlugin: (definitions: IServiceDefinition[]) => IServiceDefinition[];
 export { Point };
 export * from './interfaces';
