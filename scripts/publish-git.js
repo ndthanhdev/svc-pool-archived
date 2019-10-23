289714a275a42c5536556aa91c7920eeb1fc9a4c
ndthanhdev <ndthanhdev@outlook.com>
2019-10-24 00:16:00 +0700

    enhance publish method

--- scripts/publish-git.js
+++ scripts/publish-git.js
@@ -1,21 +1,36 @@
+const path = require('path')
 const ghPages = require('gh-pages')
 
-const base = process.cwd()
-const root = `${base}/../../`
+const rootPackage = require(`../package.json`)
 
-const package = require(`${base}/package.json`)
-const rootPackage = require(`${root}/package.json`)
+function publish(base, cb) {
+	const package = require(`${base}/package.json`)
 
-const branch = `latest/${package.name}`
-const tag = `${package.name}/${rootPackage.version}`
+	const branch = `latest/${package.name}`
+	const tag = `${package.name}/${rootPackage.version}`
 
-console.log(`publishing ${tag}`)
+	console.log(`publishing ${tag}`)
 
-ghPages.publish(
-	base,
-	{
-		branch,
-		// tag,
-	},
-	console.error,
+	return ghPages.publish(
+		base,
+		{
+			branch,
+			// tag,
+		},
+		err => {
+			console.error(err)
+			cb()
+		},
+	)
+}
+
+function publishAll(packageDirs) {
+	if (packageDirs.length) {
+		publish(packageDirs[0], () => publishAll(packageDirs.slice(1)))
+	}
+}
+
+const dirs = ['packages/core', 'packages/loader', 'packages/react'].map(s =>
+	path.resolve(s),
 )
+publishAll(dirs)
