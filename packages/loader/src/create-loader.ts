cb89ca0ab6dc2d71a1f1147f2040e42c0834c045
ndthanhdev <ndthanhdev@outlook.com>
2019-10-27 21:57:38 +0700

    catch loader excs

--- packages/loader/src/create-loader.ts
+++ packages/loader/src/create-loader.ts
@@ -14,7 +14,7 @@ export type Loader = {
 }
 
 export type AMDRequire = {
-	(deps: string[], cb: (deps: any[]) => any): void
+	(deps: string[], cb: (deps: any[]) => any, err: (error: Error) => any): void
 }
 
 function createLoader(require?: AMDRequire): Loader {
@@ -22,7 +22,7 @@ function createLoader(require?: AMDRequire): Loader {
 	const _loadAMD = require || _window.requirejs || _window.require
 
 	const _loadModule = (path: string) =>
-		new Promise(resolve => _loadAMD([path], resolve))
+		new Promise((resolve, reject) => _loadAMD([path], resolve, reject))
 
 	const load = async (config: Config) => {
 		const ps = config.pluginPaths.map(p => _loadModule(p))
