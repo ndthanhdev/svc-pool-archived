349710cf6045d5327c61d9aa211499e6af653f67
ndthanhdev <ndthanhdev@outlook.com>
2019-11-30 20:08:42 +0700

    [fix] correct useServicesHook return type

--- packages/react/src/hooks/useServices.d.ts
+++ packages/react/src/hooks/useServices.d.ts
@@ -1,13 +1,14 @@
 import React from 'react'
 import { PointNames, ValueTypeOfSvc } from '@svc-pool/core'
 import { SvcPoolContext } from '../components/Context'
+import Registry from '@svc-pool/core/registry'
 
 type useServicesHook = {
-	<T extends PointNames>(point: T): ValueTypeOfSvc<T> | undefined
+	<T extends PointNames>(point: T): Registry[T] | undefined
 }
 
 export declare function createUseServices(
-	context: React.Context<any> = SvcPoolContext,
+	context?: React.Context<any>,
 ): useServicesHook
 
 export declare const useServices: useServicesHook
